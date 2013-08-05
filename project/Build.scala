0fdd25fe2c85cb7ae9fe2f6bfb5aa3da77d6f6da
David Asabina <david@supr.nu>
2013-08-05 03:23:56 +0200

    Setting up anorm library and postgres drivers

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,8 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm
+    anorm,
+    "org.postgresql" % "postgresql" % "9.2-1003-jdbc4"
   )
 
 
