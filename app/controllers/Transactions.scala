2b97c321033723a139455f738b2d0d71dfe36aa5
David Asabina <david@supr.nu>
2013-08-21 16:31:12 +0200

    Shifting SecureAction to reusable class

--- app/controllers/Transactions.scala
+++ app/controllers/Transactions.scala
@@ -12,33 +12,7 @@ import play.api.Play.current
 import scala.util._
 import views._
 
-object Transactions extends Controller {
-
-  def SecureAction(f: Request[AnyContent] => Result): Action[AnyContent] = {
-    Action { request =>
-      val hdr = request.headers.get("x-forwarded-proto")
-      if(Play.isProd && (hdr.isDefined && StringUtils.contains(hdr.get, "https"))) {
-        request.headers.get("Authorization").flatMap { auth =>
-          auth.split(" ").drop(1).headOption.filter { encoded =>
-            new String(org.apache.commons.codec.binary.Base64.decodeBase64(encoded.getBytes)).split(":").toList match {
-              case u :: p :: Nil if u == "jim" && "smit" == p => true
-              case _ => false
-            }
-          }.map(_ => f(request))
-        }.getOrElse {
-          Unauthorized.withHeaders("WWW-Authenticate" -> """Basic realm="Secured"""")
-        }
-      } else {
-        // TODO: Redirect to SSL in Production
-        if(Play.isProd) {
-          Redirect("https://"+request.host+request.uri)
-        } else {
-          f(request)
-        }
-      }
-    }
-  }
-
+object Transactions extends BaseController {
   def index(code: String, secret: String, page: Int = 0, maxItems: Int = 100) = SecureAction { implicit request =>
     implicit val transactionWrites = new Writes[Transaction] {
       def writes(t: Transaction): JsValue = {
